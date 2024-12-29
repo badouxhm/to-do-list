6f02282e763874d0424891ea8b398387432a04a9
Rania <121013321+alivefortoolong@users.noreply.github.com>
2024-12-29 23:22:38 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -92,6 +92,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to my User Registration and Login API!');
 });
 
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
